9749a8ac03732b32d985a3f0df6890a2f9bd233a
Ankush Gedam <Ankush.Gedam@citiustech.com>
2021-10-20 17:05:28 +0530

    server configuration added

--- src/main/java/com/ct/configserver/ConfigserverApplication.java
+++ src/main/java/com/ct/configserver/ConfigserverApplication.java
@@ -2,8 +2,14 @@ package com.ct.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+@RefreshScope
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
